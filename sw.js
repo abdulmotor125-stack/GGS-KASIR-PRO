d60fb1021aa1ab1b4a44da1ba68e28f2f5174f75
abdulmotor125-stack <abdulmotor125@gmail.com>
2026-09-18 14:37:16 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'ggs-kasir-v11-10-pwa-2';
+const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(c => c.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
